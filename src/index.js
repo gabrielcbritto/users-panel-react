68ab9911c14a748704d9b0e0c1f89b8b85a61860
gabrielcbritto <gabrielcbritto@gmail.com>
2023-04-24 10:28:57 -0300

    add/split second page, changes on both styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Home from "./containers/Home";
+import Users from "./containers/Users"
 import GlobalStyle from "./globalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <App /> <GlobalStyle />
+    <Users /> <GlobalStyle />
   </>
 );
