f6ad0bed64669cb2d24d7365560fae3bf05aa9c4
gabrielcbritto <gabrielcbritto@gmail.com>
2023-04-26 18:19:32 -0300

    border radius change

--- src/components/ContainerItens/styles.js
+++ src/components/ContainerItens/styles.js
@@ -7,7 +7,7 @@ export const ContainerItens = styled.div`
     rgba(255, 255, 255, 0.6) 0.85%,
     rgba(255, 255, 255, 0.15) 100%
   );
-  border-radius: 61px 61px 0px 0px;
+  border-radius: 62px 62px 0px 0px;
   padding: 50px 36px;
   display: flex;
   flex-direction: column;
